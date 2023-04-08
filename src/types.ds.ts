c692b9b7b7cd7a71a9115562d50861d08a6277aa
Angel <aleangrodriguez123@gmail.com>
2023-04-07 21:57:45 -0500

    feat: add type for news object

---
+++ src/types.ds.ts
@@ -0,0 +1,13 @@
+type News = {
+  id: string;
+  title: string;
+  body: string;
+  imageUrl: string;
+  imageThumbnail?: string;
+  url: string;
+}
+
+type Image = {
+  url: string;
+  thumbnail: string;
+}
